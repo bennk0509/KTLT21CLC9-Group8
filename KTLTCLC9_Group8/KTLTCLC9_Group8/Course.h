7f32e6565dac6939746cc5fdcdcc1f804b667d87
PHANLAMANH <93565802+PHANLAMANH@users.noreply.github.com>
2022-03-23 00:40:02 +0700

    Update Course.h

--- KTLTCLC9_Group8/KTLTCLC9_Group8/Course.h
+++ KTLTCLC9_Group8/KTLTCLC9_Group8/Course.h
@@ -4,7 +4,7 @@
 
 void inputSemester(Semester* pSemester);
 bool CreateNewSemester(Semester* pSemester, string yearName, string semesterName);
-
+void addNewCourse(Course*& pCourse, string semesterName, string yearName, Course*& newCourse, int add);
 void updateCourseData(Course *head);
 void updateCourse(Course *head, string ID);
 void deleteCourse(Course *&head, string ID);
