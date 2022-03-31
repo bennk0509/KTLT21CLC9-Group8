e64c08b811c96a397772286a1195b4829b50e2a4
PHANLAMANH <93565802+PHANLAMANH@users.noreply.github.com>
2022-04-01 06:11:25 +0700

    Add files via upload

---
+++ KTLTCLC9_Group8/KTLTCLC9_Group8/ExportStudentListToCSV.cpp
@@ -0,0 +1,31 @@
+#include "Header.h"
+#include "Staff.h"
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <cstring>
+
+using namespace std;
+
+void exportStudentToCsv(Student* pStuInCourse, string courseID) {
+	string dirD[] = "C:\Data/File_csv/Student.csv\\";
+	string c;
+	strcat(c, dirD);
+	strcat(c, courseID);
+	strcat(c, ".csv");
+	ofstream fout;
+	fout.open(c);
+	if (fout.is_open()) {
+		int cnt = 0;
+		while (pStuInCourse) {
+			fout << ++cnt << "," << pStuInCourse->studentID << "," << pStuInCourse->Name << "," << pStuInCourse->DOB.year << "-" << pStuInCourse -> DOB.month << "-" << 
+			pStuInCourse -> DOB.day << "," << pStuInCourse->sClass << "," << pStuInCourse->gender;
+			fout << '\n'C
+			pStuInCourse = pStuInCourse->studentNext;
+		}
+		cout << "Done! The list of students has been exported into the file C:\Data/File_csv/Student.csv" << courseID << ".csv\n\n";
+		system("pause");
+		system("cls");
+	}
+	fout.close();
+}
