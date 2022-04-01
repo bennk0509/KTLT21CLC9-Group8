61e99ee5d6e6c6b688a3ba99c0bdf0f35172b75f
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2022-04-01 17:33:11 +0700

    ViewScoreBoard
    chưa debug, fix code dùm

--- KTLTCLC9_Group8/KTLTCLC9_Group8/Staff.h
+++ KTLTCLC9_Group8/KTLTCLC9_Group8/Staff.h
@@ -6,6 +6,10 @@
 void viewClassesList(Year *pYear);
 void viewStudentListInCourse(Student *head);
 void CreateNewYear(Year*& pYear, string yearname);
+<<<<<<< Updated upstream
 void exportStudentToCsv(Student* pStuInCourse, string courseID);
 
+=======
+void ViewScoreBoardOfCourse(Course *pCourse,string courseID);
+>>>>>>> Stashed changes
 #endif // ! _STUDENT_H
